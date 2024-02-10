c520883f5bd6a66af4a4b2fa3b8a489296db76e6
october03 <season_autumn@kakao.com>
2024-02-10 11:14:38 +0900

    Initial Commit

---
+++ src/main/kotlin/com/october03/cafe/kiosk/payment/KioskPaymentServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.october03.cafe.kiosk.payment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KioskPaymentServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<KioskPaymentServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/october03/cafe/kiosk/payment/KioskPaymentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.october03.cafe.kiosk.payment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KioskPaymentServiceApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
